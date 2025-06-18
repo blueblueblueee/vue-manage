f28a85d7d620aca503ac4770a71d96f211c89b2f
cedric <2361751078@qq.com>
2025-06-18 15:06:40 +0800

    feat:增加菜单，折叠按钮

--- src/views/layout/HeaderView.vue
+++ src/views/layout/HeaderView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header-view">
-    <Breadcrumb />
+    <el-button icon="el-icon-s-unfold" v-if="!isShow" v-show="!isShow" @click="isShow=!isShow"></el-button>
+    <el-button icon="el-icon-s-fold" v-else v-show="isShow" @click="isShow=!isShow"></el-button>
   </div>
 </template>
 
@@ -12,7 +13,12 @@ export default {
   },
   data() {
     return {
-
+      isShow: true,
+    }
+  },
+  methods: {
+    isShow() {
+      this.isShow = !this.isShow;
     }
   }
 }
@@ -22,5 +28,12 @@ export default {
 .header-view {
   height: 80px;
   box-shadow: 0 5px 5px #ddd;
+  button {
+    width: 50px;
+    height: 50px;
+    font-size: 24px;
+    border: none;
+    padding: 12px 10px;
+  }
 }
 </style>
